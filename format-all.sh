0f14607f007043e754c3de1941f611b2c041a04c
Marek Rusinowski <marekrusinowski@gmail.com>
2017-01-13 15:20:46 +0100

    improve format-all.sh

--- format-all.sh
+++ format-all.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-GIT_ROOT="`git rev-parse --git-dir`/.."
+cd $(dirname $(realpath "$0"))
 
-find "${GIT_ROOT}/src" \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' -o -name -'*.c' \) -print0 | xargs -n 1 -P 0 -0 clang-format -style=file -i
+num_cores=$(nproc 2> /dev/null || sysctl -n hw.ncpu 2> /dev/null || echo 4)
+
+find src \
+     -regex '.*\.\(cpp\|hpp\|c\|h\)' \
+     -print0 | \
+     xargs -n 1 -P $num_cores -0 clang-format -style=file -i
